d779e1c33039cbe76e730714f2aeede43e6e4f57
Hersan Kuang <mlnrmsw@gmail.com>
2022-11-09 21:05:52 +0800

    feat: encapsulate the network request class

---
+++ services/index.js
@@ -0,0 +1,27 @@
+// 封装成类 -> 实例
+class HXRequest {
+  constructor(baseURL) {
+    this.baseURL = baseURL
+  }
+  request(options) {
+    const { url } = options
+    return new Promise((resolve, reject) => {
+      wx.request({
+        ...options,
+        url: this.baseURL + url,
+        success: (res) => {
+          resolve(res.data)
+        },
+        fail: reject
+      })
+    })
+  }
+  get(options) {
+    return this.request({ ...options, method: "get" })
+  }
+  post(options) {
+    return this.request({ ...options, method: "post" })
+  }
+}
+
+export const hxRequest = new HXRequest("http://codercba.com:9002")
