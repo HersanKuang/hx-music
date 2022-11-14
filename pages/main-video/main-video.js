7dcf958eba362e0890eed0f5a4947d3a05916d08
Hersan Kuang <mlnrmsw@gmail.com>
2022-11-14 21:14:26 +0800

    feat: pull up to load more

--- pages/main-video/main-video.js
+++ pages/main-video/main-video.js
@@ -3,7 +3,8 @@ import { getTopMv } from "../../services/video"
 
 Page({
   data: {
-    videoList: []
+    videoList: [],
+    hasMore: true
   },
   onLoad() {
     // 发送网络请求
@@ -16,7 +17,16 @@ Page({
     // getTopMv().then(res => {
     //   this.setData({ videoList: res.data })
     // })
-    const res = await getTopMv()
-    this.setData({ videoList: res.data })
+    const res = await getTopMv(this.data.videoList.length)
+    const newVideoList = [...this.data.videoList, ...res.data]
+    this.setData({ videoList: newVideoList })
+    this.data.hasMore = res.hasMore
+  },
+
+  // 监听上拉和下拉功能
+  onReachBottom() {
+    // 判断是否有更多的数据，最多50条
+    if (!this.data.hasMore) return
+    this.fetchTopMV()
   }
 })
