5d62b3e3c1d0ee34089fdfa39ce401b528ff9b84
Hersan Kuang <mlnrmsw@gmail.com>
2022-11-18 00:27:37 +0800

    feat: add share to moments feature

--- pages/main-music/main-music.js
+++ pages/main-music/main-music.js
@@ -3,4 +3,18 @@ Page({
   data: {
     searchValue: ''
   },
+  onShareAppMessage() {
+    return {
+      title: '啾咪音乐',
+      path: 'pages/main-music/main-music',
+      imageUrl: 'https://hersan.cn/%E5%9B%BE%E7%89%87/%E7%B2%BE%E7%BE%8E%E5%9B%BE%E7%89%87/%E4%B8%87%E5%9C%A3.jpg'
+    }
+  },
+  onShareTimeline() {
+    return {
+      title: '啾咪音乐',
+      path: 'pages/main-music/main-music',
+      imageUrl: 'https://hersan.cn/%E5%9B%BE%E7%89%87/%E7%B2%BE%E7%BE%8E%E5%9B%BE%E7%89%87/%E4%B8%87%E5%9C%A3.jpg'
+    }
+  }
 })
